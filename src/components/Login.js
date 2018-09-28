5fa3e466b781d0c638805488b7aa7f6c9203e7a3
Roland Balogh <roland.balogh@jaystack.com>
2018-09-28 15:04:22 +0200

    radio

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,6 +7,7 @@ class Login extends Component {
     this.state = {
       room: '',
       name: '',
+      radio: 'ai',
     }
 
     this.handleChange = event => {
@@ -18,12 +19,19 @@ class Login extends Component {
       this.props.loginUser(room, name);
       event.preventDefault();
     }
+
+    this.handleChange = event => {
+      console.log(event.target.name);
+      this.setState({ radio: event.target.name });
+    }
   }
   render() {
     return (
       <form className="login">
         <label htmlFor="game"><input name="game" id="game" placeholder="Room name" onChange={this.handleChange} /></label>
         <label htmlFor="name"><input name="name" id="name" placeholder="Player name" onChange={this.handleChange} /></label>
+        <input type="radio" name="ai" value="ai" id="ai" style={{width: "20%"}} onChange={this.handleChange} /><label htmlFor="ai" >AI</label>
+        <input type="radio" name="human" value="human" id="human" style={{width: "20%"}} onChange={this.handleChange} /><label htmlFor="human" >Human</label>
         <div><button type="submit" className="btn btn-info" onClick={this.handleLogin}>Start</button></div>
       </form>
     );
