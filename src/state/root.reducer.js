6417f51f6416f63c0adff3a54cfcb7a88f146240
Imre Kiss <imre.kiss@jaystack.com>
2018-09-28 13:50:25 +0200

    Add game reducer to the store

--- src/state/root.reducer.js
+++ src/state/root.reducer.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import { reducer as user } from './user';
 import { reducer as app } from './app';
 import { reducer as ships } from './ships';
+import { reducer as game } from './game';
 
 export default combineReducers({
   app,
   ships,
-  user
+  user,
+  game
 });
