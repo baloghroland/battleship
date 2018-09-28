e17d218000e2690afb868932d2afc75dc17c4d38
Imre Kiss <imre.kiss@jaystack.com>
2018-09-28 12:14:47 +0200

    Add addUserShip action

--- src/state/game/index.js
+++ src/state/game/index.js
@@ -3,24 +3,34 @@ import { createAction, handleActions } from 'redux-actions';
 /**
  * INITIAL STATE
  */
-export const initialState = {};
+export const initialState = {
+  userShips: []
+};
 
 /**
  * ACTION TYPES
  */
+export const ADD_USER_SHIP = 'ADD_USER_SHIP';
 
 /**
  * ACTION CREATORS
  */
+export const addUserShip = createAction(
+  ADD_USER_SHIP,
+  (x, y, size, orientation) => ({ x, y, size, orientation })
+);
 
 /**
  * SELECTORS
  */
+export const getUserShips = state => state.game.userShips;
 
 /**
  * REDUCER
  */
 export const reducer = handleActions(
-  {},
+  {
+    [addUserShip]: (state, { payload: { x, y, size, orientation } }) => ({ ...state, userShips: [...state.userShips, { x, y, size, orientation }] })
+  },
   initialState
 );
