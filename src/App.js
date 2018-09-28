825e77426324a756a77109c92eee7caa7d009cd5
tbarna-jay <tamas.barna@jaystack.com>
2018-09-28 11:05:04 +0200

    lol

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/Login.js';
+import Game from './components/Game.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="App-title">Welcome to Battleship</h1>
-        <Login />
+      <div className="App container">
+        <div className="main well">
+          <h1 className="App-title">Welcome to Battleship</h1>
+          <Login />
+          <Game />
+        </div>
       </div>
     );
   }
